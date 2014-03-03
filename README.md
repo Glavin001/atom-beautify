f9d1db1d3c012c2798ad4b6da78523a13d4972d6
Donald Pipowitch <pipo@senaeh.de>
2014-03-03 08:51:16 +0100

    first release of atom-beautify, add support for 'indent_size' and 'indent_char' for HTML/CSS/JS

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-atom-beautify
-=============
+# Beautify
 
-Beautify HTML, CSS and Javascript in Atom
+[Atom package](https://github.com/einars/js-beautify)
+
+> [Beautify](https://github.com/mishoo/UglifyJS2) HTML, CSS and Javascript in Atom.
+
+
+## Usage
+
+Open the Command Palette, and type `Beautify`. This will beautify JS, HTML or CSS files. It will only beautify selected text, if a selection is found - if not, the whole file will be beautified.
+
+
+## License
+
+MIT © [Donald Pipowitch](https://github.com/donaldpipowitch)
---
+++ index.js
@@ -0,0 +1,51 @@
+/* global atom */
+
+'use strict';
+
+var beautifyJS = require('js-beautify');
+var beautifyHTML = require('js-beautify').html;
+var beautifyCSS = require('js-beautify').css;
+
+function beautify() {
+  var text;
+  var editor = atom.workspace.getActiveEditor();
+  var isSelection = !! editor.getSelectedText();
+  var softTabs = editor.softTabs;
+  var tabLength = editor.getTabLength();
+
+  var beautifyOptions = {
+    'indent_size': softTabs ? tabLength : 1,
+    'indent_char': softTabs ? ' ' : '\t'
+  };
+
+  if (isSelection) {
+    text = editor.getSelectedText();
+  } else {
+    text = editor.getText();
+  }
+
+  switch (editor.getGrammar().name) {
+    case 'JavaScript':
+      text = beautifyJS(text, beautifyOptions);
+      break;
+    case 'HTML':
+      text = beautifyHTML(text, beautifyOptions);
+      break;
+    case 'CSS':
+      text = beautifyCSS(text, beautifyOptions);
+      break;
+  }
+
+  if (isSelection) {
+    editor.setTextInBufferRange(
+      editor.getSelectedBufferRange(),
+      text
+    );
+  } else {
+    editor.setText(text);
+  }
+}
+
+exports.activate = function() {
+  return atom.workspaceView.command('beautify', beautify);
+};
