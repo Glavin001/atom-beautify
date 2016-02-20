f4000cc990c8644dfb2f2595d5c103fde9265c9a
Jon Crenshaw <jon@redteal.com>
2016-02-19 20:25:11 -0800

    Add examples for Apex language

---
+++ examples/nested-jsbeautifyrc/apex/expected/test.java
@@ -0,0 +1,19 @@
+class Aligns {
+        final int SZF = 4;
+        final int SZ2F = 4;
+        final int aBarF;
+        final int[] someMoreIntsF;
+        final Tem<Plate> edVarF;
+        final int aBarSetF=null;
+        final int[] someMoreIntsSetF=null;
+        final Tem<Plate> edVarF=null;
+        int SZ = 4;
+        int SZ2 = 4;
+        int aBar;
+        int spacer;
+        int[] someMoreInts;
+        Tem<Plate> edVar;
+        int aBarSet=null;
+        int[] someMoreIntsSet=null;
+        Tem<Plate> edVar=null;
+}
