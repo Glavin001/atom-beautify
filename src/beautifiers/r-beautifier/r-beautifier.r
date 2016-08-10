dd817333292dd18464a5df9b8327eef6fe755080
Sebastian DiLorenzo <dilorenzo.sebastian@gmail.com>
2016-08-10 15:26:32 +0200

    added code to r-beautifier.r that installs formatR if the package does not exist.

--- src/beautifiers/r-beautifier/r-beautifier.r
+++ src/beautifiers/r-beautifier/r-beautifier.r
@@ -1,3 +1,4 @@
+if(!("formatR" %in% installed.packages()[,"Package"])) install.packages("formatR",repos='http://cran.us.r-project.org')
 library(formatR)
 file <- commandArgs(trailingOnly = TRUE)[1]
 tidy_source(text = readLines(file))
