3435e16eb45fe1cb8bdbc5f47a1a3df1e944198a
Glavin Wiechert <glavin.wiechert@gmail.com>
2017-04-15 16:03:56 -0300

    Fixes #1557. Fix Vue beautifier adding extra newlines

---
+++ examples/nested-jsbeautifyrc/vue/expected/styles.vue
@@ -0,0 +1,29 @@
+
+<style lang="sass">
+nav {
+    ul {
+        margin:0 padding: 0;
+    }
+    li {
+        display: inline-block;
+    }
+    a {
+        display: block;
+    }
+}
+</style>
+
+<style lang="scss">
+nav {
+    ul {
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+    }
+    a {
+        display: block;
+    }
+}
+</style>
---
+++ examples/nested-jsbeautifyrc/vue/original/styles.vue
@@ -0,0 +1,28 @@
+
+<style lang="sass">
+nav {
+ul {
+    margin: 0
+    padding: 0
+}
+  li {
+    display: inline-block
+}
+a {
+    display: block
+    }
+}
+</style>
+
+<style lang="scss">
+nav {
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li { display: inline-block; }
+  a {
+    display: block;
+  }
+}
+</style>
