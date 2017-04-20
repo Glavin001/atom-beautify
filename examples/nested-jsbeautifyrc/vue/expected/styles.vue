de6323e20234e4e5fc131b9940ed9a0175bc41c8
Glavin Wiechert <glavin.wiechert@gmail.com>
2017-04-19 23:16:20 -0300

    Fix Vue test

--- examples/nested-jsbeautifyrc/vue/expected/styles.vue
+++ examples/nested-jsbeautifyrc/vue/expected/styles.vue
@@ -1,14 +1,15 @@
 
 <style lang="sass">
 nav {
-    ul {
-        margin:0 padding: 0;
-    }
-    li {
-        display: inline-block;
-    }
-    a {
-        display: block;
+ul {
+    margin: 0
+    padding: 0
+}
+  li {
+    display: inline-block
+}
+a {
+    display: block
     }
 }
 </style>
