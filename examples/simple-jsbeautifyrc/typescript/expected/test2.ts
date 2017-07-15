87f35c895a7093b6b043d651a1a527704880f58b
kamontat <kamontat_c@hotmail.com>
2017-07-15 23:07:02 +0700

    update new test-case

--- examples/simple-jsbeautifyrc/typescript/expected/test2.ts
+++ examples/simple-jsbeautifyrc/typescript/expected/test2.ts
@@ -1,56 +1,55 @@
 module XYZ {
-  interface I {
-    x: number,
-   y,
-    z
-}
-
-var zZz = {
-  a: 123,
-  b: 456
-};
-
-enum En {
-  AB,
-  CD
-}
-
-var y = 3;
-
-switch (y {
-  case X:
-    console.log("x");
-    break;
-  default:
-    console.log("y");
-}
-
-if (x === 3) {
-  if (y === 4) {
-    console.log("hey");
-  }
-  console.log("x");
-  x = y.hey
-    .b
-    .c
-    .d;
-
-
-  export class X {
-    private x: number;
-
-    constructor() {
-      console.log("hi");
+    interface I {
+        x: number,
+        y,
+        z
     }
 
-    public mem() {
-      console.log("yes");
-    }
-  }
+    var zZz = {
+        a: 123,
+        b: 456
+    };
 
-  function xyz() {
-    function mem() {
-      console.log("yes");
+    enum En {
+        AB,
+        CD
     }
-  }
-}
+
+    var y = 3;
+
+    switch (y {
+        case X:
+            console.log("x");
+            break;
+        default:
+            console.log("y");
+    }
+
+    if (x === 3) {
+        if (y === 4) {
+            console.log("hey");
+        }
+        console.log("x");
+        x = y.hey
+            .b
+            .c
+            .d;
+
+        export class X {
+            private x: number;
+
+            constructor() {
+                console.log("hi");
+            }
+
+            public mem() {
+                console.log("yes");
+            }
+        }
+
+        function xyz() {
+            function mem() {
+                console.log("yes");
+            }
+        }
+    }
