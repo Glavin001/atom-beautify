8d846c6aa71ec661a2e97baf85d0957186b1956e
kamontat <kamontat_c@hotmail.com>
2017-07-15 23:47:17 +0700

    expected should indent by 2

--- examples/simple-jsbeautifyrc/typescript/expected/test2.ts
+++ examples/simple-jsbeautifyrc/typescript/expected/test2.ts
@@ -1,55 +1,55 @@
 module XYZ {
-    interface I {
-        x: number,
-        y,
-        z
+  interface I {
+    x: number,
+    y,
+    z
+  }
+
+  var zZz = {
+    a: 123,
+    b: 456
+  };
+
+  enum En {
+    AB,
+    CD
+  }
+
+  var y = 3;
+
+  switch (y {
+    case X:
+      console.log("x");
+      break;
+    default:
+      console.log("y");
+  }
+
+  if (x === 3) {
+    if (y === 4) {
+      console.log("hey");
+    }
+  console.log("x");
+  x = y.hey
+    .b
+    .c
+    .d;
+
+    export class X {
+      private x: number;
+
+      constructor() {
+        console.log("hi");
+      }
+
+      public mem() {
+        console.log("yes");
+      }
     }
 
-    var zZz = {
-        a: 123,
-        b: 456
-    };
-
-    enum En {
-        AB,
-        CD
+    function xyz() {
+      function mem() {
+        console.log("yes");
+      }
     }
-
-    var y = 3;
-
-    switch (y {
-        case X:
-            console.log("x");
-            break;
-        default:
-            console.log("y");
-    }
-
-    if (x === 3) {
-        if (y === 4) {
-            console.log("hey");
-        }
-        console.log("x");
-        x = y.hey
-            .b
-            .c
-            .d;
-
-        export class X {
-            private x: number;
-
-            constructor() {
-                console.log("hi");
-            }
-
-            public mem() {
-                console.log("yes");
-            }
-        }
-
-        function xyz() {
-            function mem() {
-                console.log("yes");
-            }
-        }
-    }
+  }
