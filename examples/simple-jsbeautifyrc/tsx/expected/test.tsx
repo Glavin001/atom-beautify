7107f00beb0f0f06004b8356a5f29c2121fbe76e
Steven Zeck <steven.zeck@gmail.com>
2018-02-06 10:32:09 -0600

    Fix unit test for tsx

--- examples/simple-jsbeautifyrc/tsx/expected/test.tsx
+++ examples/simple-jsbeautifyrc/tsx/expected/test.tsx
@@ -1,7 +1,7 @@
 class Test extends React.Component<Foo> {
   render() {
     return (
-      <div className="class" >
+      <div className= "class" >
       <h2 className="anotherClass" >
         { this.foo.bar }
         < /h2>
