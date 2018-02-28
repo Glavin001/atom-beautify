1ceb96ef516a92f13c1b151eff4e2c1c750dc054
Steven Zeck <steven.zeck@gmail.com>
2018-02-27 21:38:33 -0600

    Rework jest config

--- spec/atombeautify.spec.js
+++ spec/atombeautify.spec.js
@@ -1,15 +1,16 @@
 describe("Atom Beautify", () => {
 
+  beforeEach(async () => {
+    jest.setTimeout(10000);
+    await atom.packages.activatePackage("atom-beautify");
+  });
+
+  it("atom-beautify package is activated", () => {
+    expect(atom.packages.isPackageActive("atom-beautify")).toEqual(true);
+  });
+
   it("Jest test", () => {
     expect(true).toEqual(true);
   });
-
-  // beforeEach(async () => {
-  //   await atom.packages.activatePackage("atom-beautify");
-  // });
-  //
-  // it("atom-beautify package is activated", () => {
-  //   expect(atom.packages.isPackageActive("atom-beautify")).toEqual(true);
-  // });
 
 });
