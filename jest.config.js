44be76cab45ffe7b2517d8d38dc3a6ef6d505e6b
Steven Zeck <steven.zeck@gmail.com>
2018-02-28 17:13:08 -0600

    Redo jest config again

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverage: true,
+  coverageReporters: ["json", "lcov", "text", "html"],
+  preset: "jest-preset-atom"
+};
