42e493ea52298a72651878d2328fa0497dd50598
Steven Zeck <steven.zeck@gmail.com>
2018-03-09 22:49:51 -0600

    Enable SQL for CI tests

--- examples/simple-jsbeautifyrc/sql/expected/test.sql
+++ examples/simple-jsbeautifyrc/sql/expected/test.sql
@@ -3,7 +3,7 @@ SELECT ca.proj_id AS proj_id,
        ca.ca_date_start AS proj_start,
        ca.ca_date_end AS proj_end,
 
-  (SELECT count(*)
+  (SELECT COUNT(*)
    FROM rotations r
    WHERE r.proj_id = proj_id
      AND r.r_status = 'R'
@@ -12,7 +12,7 @@ SELECT ca.proj_id AS proj_id,
   (SELECT count(*)
    FROM rotations r
    WHERE r.proj_id = proj_id
-     AND r.channel_id = 24) r_rtb_count
+     AND r.channel_id = 24 ) r_rtb_count
 FROM projs ca,
      clients c,
      proj_auth caa
@@ -20,6 +20,6 @@ WHERE ca.client_id = 12345
   AND ca.client_id = c.client_id
   AND ca_type = 'zzz'
   AND c.agency_id = 0
-  AND ca.client_id = nvl(caa.client_id, ca.client_id)
-  AND proj_id = nvl(caa.proj_id, proj_id)
+  AND ca.client_id = NVL(caa.client_id, ca.client_id)
+  AND proj_id = NVL(caa.proj_id, proj_id)
   AND caa.contact_id = 7890
