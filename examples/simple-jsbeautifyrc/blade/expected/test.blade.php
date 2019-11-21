793c63e33135690c54b137eccba9e09baa1cf26c
Adam Nielsen <1765602+iwasherefirst2@users.noreply.github.com>
2019-11-21 15:36:24 +0100

    Fix expected test file

--- examples/simple-jsbeautifyrc/blade/expected/test.blade.php
+++ examples/simple-jsbeautifyrc/blade/expected/test.blade.php
@@ -1,7 +1,38 @@
-<script>
-  @foreach ($users as $user) {
-      {
-        $user - > name
-      }
-    } @endforeach
+<html>
+
+<head>
+  <title>App Name - @yield('title')</title>
+</head>
+<script type="text/javascript">
+  document.write("@" );
 </script>
+
+<body>
+  @section('sidebar')
+  This is the {{ $mater }} sidebar.
+  @show
+
+  <div class="container">
+    @yield('content')
+  </div>
+  <p><span>@lang('Hi')</span></p>
+
+  @component('alert')
+  @slot('title')
+  Forbidden
+  @endslot
+  <strong>Whoops!</strong> Something went wrong!
+  @endcomponent
+
+  @foreach ($users as $user)
+  @if ($user->type == 1)
+  @continue
+  @endif
+  <li>{{ $user->name }}</li>
+  @if ($user->number == 5)
+  @break
+  @endif
+  @endforeach
+</body>
+
+</html>
